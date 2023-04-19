9bcd2d4e0747acabe741acbca9459815b4cd93de
chuuuchin <127931308+chuuuchin@users.noreply.github.com>
2023-04-19 07:21:58 +0000

    ok

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# © Чучин Д. М., Богданов М. Ю., 2023
+### * Copyright Chuchin Dmitriy Maksimovich - All Rights Reserved
+### * Unauthorized copying of any files, via any medium is strictly prohibited
+### * Proprietary and confidential
+### * Written by Dmitriy Chuchin <chuuchin@yandex.ru>, April 2023
+###
 
 import telebot
 import random
